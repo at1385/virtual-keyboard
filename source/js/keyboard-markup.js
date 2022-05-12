084dabc33dd31291ae8a17f464b505d5755362d7
Alexander Tikhonov <at1385@yandex.ru>
2022-05-12 13:27:54 +0500

    feat: add RU layout markup to index.html

--- source/js/keyboard-markup.js
+++ source/js/keyboard-markup.js
@@ -1,49 +1,53 @@
 import virtualKeyboardContainer from './container-markup.js';
-import keys from './data.js';
+import keyboard from './data.js';
 
 const virtualKeyboardKeysContainer = document.createElement('div');
 virtualKeyboardKeysContainer.className = 'virtual-keyboard__keys';
 virtualKeyboardContainer.append(virtualKeyboardKeysContainer);
 
-const createVirtualKeyboardKey = (key) => {
+const createVirtualKeyboardKey = (key, layout, isHidden) => {
   const virtualKeyboardKey = document.createElement('button');
 
   switch (key) {
     case 'Backspace':
     case 'Enter':
-    case '<span class="visually-hidden">Left </span>Shift':
-      virtualKeyboardKey.className = 'virtual-keyboard__key virtual-keyboard__key--four-columns';
+    case 'Shift<span class="visually-hidden">Left</span>':
+      virtualKeyboardKey.className = `virtual-keyboard__key virtual-keyboard__key--four-columns ${layout}`;
       break;
     case 'Caps Lock':
-      virtualKeyboardKey.className = 'virtual-keyboard__key virtual-keyboard__key--four-columns virtual-keyboard__key--off';
+      virtualKeyboardKey.className = `virtual-keyboard__key virtual-keyboard__key--four-columns virtual-keyboard__key--off ${layout}`;
       break;
     case 'Tab':
-    case 'Del':
-    case '<span class="visually-hidden">Left </span>Ctrl':
-    case '<span class="visually-hidden">Right </span>Ctrl':
-      virtualKeyboardKey.className = 'virtual-keyboard__key virtual-keyboard__key--three-columns';
+    case 'Delete':
+    case 'C<span class="visually-hidden">on</span>tr<span class="visually-hidden">o</span>l<span class="visually-hidden">Left</span>':
+    case 'C<span class="visually-hidden">on</span>tr<span class="visually-hidden">o</span>l<span class="visually-hidden">Right</span>':
+      virtualKeyboardKey.className = `virtual-keyboard__key virtual-keyboard__key--three-columns ${layout}`;
       break;
     case ' ':
-      virtualKeyboardKey.className = 'virtual-keyboard__key virtual-keyboard__key--twelve-columns';
+      virtualKeyboardKey.className = `virtual-keyboard__key virtual-keyboard__key--twelve-columns ${layout}`;
       break;
     default:
-      virtualKeyboardKey.className = 'virtual-keyboard__key virtual-keyboard__key--two-column';
+      virtualKeyboardKey.className = `virtual-keyboard__key virtual-keyboard__key--two-column ${layout}`;
   }
 
   virtualKeyboardKey.setAttribute('type', 'button');
+  if (isHidden) virtualKeyboardKey.classList.add('hidden');
   virtualKeyboardKey.innerHTML = `${key}`;
 
   return virtualKeyboardKey;
 };
 
-const renderVirtualKeyboardKeys = () => {
+const renderVirtualKeyboardKeys = (keyData, layout, isHidden = false) => {
   const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < keys.length; i += 1) {
-    fragment.append(createVirtualKeyboardKey(keys[i]));
-  }
+  Object.values(keyData).forEach((key) => {
+    fragment.append(createVirtualKeyboardKey(key, layout, isHidden));
+  });
 
   virtualKeyboardKeysContainer.append(fragment);
 };
 
-renderVirtualKeyboardKeys();
+renderVirtualKeyboardKeys(keyboard.en.default, 'en');
+renderVirtualKeyboardKeys(keyboard.ru.default, 'ru', true);
+
+export default virtualKeyboardContainer;
